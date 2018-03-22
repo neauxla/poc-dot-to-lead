703d7e3c110b491fec7971674f762b92def2ee41
David Entremont <dentremont@deloitte.com>
2018-03-22 11:44:49 -0600

    Use EJS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-/* -- Require Scripts -- */
-let app = require('express');
-let views = require('views');
+// Require Scripts
+let app = require('express')();
+let ejs = require('ejs');
 let nforce = require('nforce');
 let DOTService = require('./services/CompanyData');
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 console.log('Hellerrrr');
