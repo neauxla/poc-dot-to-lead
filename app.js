bde94bd1251456ee609ff39ba30ec8d3f06f987c
David Entremont <dentremont@deloitte.com>
2018-03-22 13:08:05 -0600

    Setup for Heroku

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post('/', function(req, res) {
     
 })
 
-app.listen(3000, function() {
-    console.log('App running on port 3000');
+app.listen(process.env.PORT, function() {
+    console.log('App running on port: ' + process.env.PORT);
 })
